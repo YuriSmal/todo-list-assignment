863a5ca5b31a16f79937eedd2b5e43580afab1be
Yurii Smal <ysmal@logitech.com>
2025-06-17 20:34:01 +0300

    Add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/todo-board-assignment',
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
